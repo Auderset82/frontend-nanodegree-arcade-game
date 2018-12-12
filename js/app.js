4b26b2ab2a504c78703e6323e74d747c32826651
Christian <christian.auderset@alphasquare.ch>
2018-12-12 12:15:22 +0100

    Working with Time

--- js/app.js
+++ js/app.js
@@ -1,30 +1,20 @@
-
 let timer_element = document.querySelector('.time');
 let currentTimer;
-//resetTimer(currentTimer);
 seconds = 0;
-currentTimer = setInterval(incrementSeconds, 10);
+currentTimer = setInterval(initTime, 10);
 let best_time_element = document.querySelector('.best-time');
 let best_time = 0;
 best_time_element.innerHTML = best_time;
 
-
-function incrementSeconds() {
-    seconds += 1;
-    timer_element.innerText = seconds;
-}
-
 function initTime() {
-    currentTimer = setInterval(function() {
-      //  $time.text(`${second}`)
-        second = second + 1
-    }, 10);
+	seconds = seconds + 1;
+	timer_element.innerHTML = seconds;
 }
 
-function resetTimer(timer) {
-    if (timer) {
-        clearInterval(timer);
-    }
+initTime();
+
+function resetTimer() {
+	clearInterval(currentTimer);
 }
 
 // Enemies our player must avoid
@@ -37,31 +27,31 @@ function resetTimer(timer) {
 //     this.sprite = 'images/enemy-bug.png';
 // };
 class Enemy {
-    constructor(x, y, speed) {
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.sprite = 'images/enemy-bug.png';
-    }
-    update(dt) {
-        if (this.x > 550) {
-            this.x = -45;
-        }
-        this.x = (this.x + this.speed * dt);
+	constructor(x, y, speed) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+		this.sprite = 'images/enemy-bug.png';
+	}
+	update(dt) {
+		if (this.x > 550) {
+			this.x = -45;
+		}
+		this.x = (this.x + this.speed * dt);
 
-        if (Math.abs(this.x - player.x) < 45 && Math.abs(this.y - player.y) < 45) {
-            player.x = 202;
-            player.y = 405;
-            initTime();
-            resetTimer(currentTimer);
-            seconds = 0;
-            currentTimer = setInterval(incrementSeconds, 10);
-        }
-    }
+		if (Math.abs(this.x - player.x) < 45 && Math.abs(this.y - player.y) < 45) {
+			resetTimer(currentTimer);
+			seconds = 0;
+			currentTimer = setInterval(initTime, 10);
+			initTime();
+			player.x = 202;
+			player.y = 405;
+		}
+	}
 
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
+	render() {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
 }
 
 //Generation of our Enemies
@@ -70,81 +60,73 @@ enemy2 = new Enemy(0, 147, 33);
 enemy3 = new Enemy(0, 230, 95);
 enemy4 = new Enemy(0, 230, 140);
 
-
 // Constructor of our Player
 class Player {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.player = 'images/char-boy.png';
-    }
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.player = 'images/char-boy.png';
+	}
 
-    update(dt) {}
+	update(dt) {}
 
-    render() {
-        ctx.drawImage(Resources.get(this.player), this.x, this.y);
+	render() {
+		ctx.drawImage(Resources.get(this.player), this.x, this.y);
+	}
 
-    }
+	handleInput(keyPress) {
+		if (keyPress == 'left' && this.x > 0) {
+			this.x = this.x - 102
+		}
+		if (keyPress == 'right' && this.x < 360) {
+			this.x = this.x + 102;
+		}
+		if (keyPress == 'down' && this.y > 0) {
+			this.y = this.y - 83;
+		}
+		if (keyPress == 'up' && this.y < 405) {
+			this.y = this.y + 83;
+		}
+		if (this.y < 0) {
+			check_besttime();
 
-    handleInput(keyPress) {
-        if (keyPress == 'left' && this.x > 0) {
-            this.x = this.x - 102
-        }
-        if (keyPress == 'right' && this.x < 360) {
-            this.x = this.x + 102;
-        }
-        if (keyPress == 'down' && this.y > 0) {
-            this.y = this.y - 83;
-        }
-        if (keyPress == 'up' && this.y < 405) {
-            this.y = this.y + 83;
-        }
-        if (this.y < 0) {
-            check_besttime();
-            initTime();
-            resetTimer(currentTimer);
-
-            setTimeout(function() {
-                player.x = 202;
-                player.y = 405;
-
-                seconds = 0;
-                currentTimer = setInterval(incrementSeconds, 10);
-            }, 500);
-
-
-        }
-    }
+			setTimeout(function () {
+				player.x = 202;
+				player.y = 405;
+				resetTimer(currentTimer);
+				seconds = 0;
+				currentTimer = setInterval(initTime, 10);
+				initTime();
+				//currentTimer = setInterval(incrementSeconds, 10);
+			}, 500);
+		}
+	}
 }
 
-function check_besttime () {
-//  best_time = best_time_element.value;
-  if (best_time == 0) {
-    best_time = seconds;
-    best_time_element.innerHTML = best_time;
-  }
-  else {
-    if (best_time > seconds) {
-      best_time = seconds;
-      best_time_element.innerHTML = best_time;
-    }
-
-  }
+function check_besttime() {
+	if (best_time == 0) {
+		best_time = seconds;
+		best_time_element.innerHTML = best_time;
+	} else {
+		if (best_time > seconds) {
+			best_time = seconds;
+			best_time_element.innerHTML = best_time;
+		}
+	}
 }
 
 //Generation of our Player
 player = new Player(202, 410);
 
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'down',
-        39: 'right',
-        40: 'up'
-    };
+document.addEventListener('keyup', function (e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'down',
+		39: 'right',
+		40: 'up'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
